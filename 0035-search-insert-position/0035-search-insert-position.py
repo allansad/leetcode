ebde4d62fc9101c1e100e5b50189221adfb7387a
Yi-seong <66365251+allansad@users.noreply.github.com>
2024-11-09 23:04:11 +0900

    Time: 0 ms (100%), Space: 12 MB (98.48%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        # 시간복잡도: 이진 탐색이기 때문에 nums의 길이에 대해 O(logn)
+        # 공간복잡도: O(1)
+
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        
+        return left
