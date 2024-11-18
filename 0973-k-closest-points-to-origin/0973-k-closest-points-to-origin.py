f84e27a980e85e8d8268c0f2510fdc31a7c9d031
Yi-seong <66365251+allansad@users.noreply.github.com>
2024-11-18 21:46:21 +0900

    Time: 83 ms (72.01%), Space: 17.1 MB (87.57%) - LeetHub

---
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def kClosest(self, points, k):
+        """
+        :type points: List[List[int]]
+        :type k: int
+        :rtype: List[List[int]]
+        """
+        # 시간복잡도: 
+        # 공간복잡도: 
+        temp_arr = []
+        result_arr = []
+        
+        for i, point in enumerate(points):
+            temp_arr.append((i, pow(point[0], 2)+pow(point[1], 2)))
+        
+        temp_arr.sort(key=lambda point: point[1])
+        
+        for i in range(k):
+            result_arr.append(points[temp_arr[i][0]])
+            
+        return result_arr
